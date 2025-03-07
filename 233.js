8e0f070db10e071800212e15ad42040555193da2
Saatvik Rai <89719423+NiNjA4559@users.noreply.github.com>
2025-03-07 19:14:24 +0530

    Update 233.js

--- 233.js
+++ 233.js
@@ -2,16 +2,18 @@
  * @param {number} n
  * @return {number}
  */
-var countDigitOne = function(n) {
+var countDigitOne = function (n) {
     let ones = 0;
     n = `${n}`;
-    n.split("").forEach((str, i) => {
-        if(str === "0") return;
-        if(str === "1") {
-            ones += parseInt(n)%Math.pow(10, n.length-i-1) + 1 + (n.length-i-1)*Math.pow(10, n.length-i-2);
-        } else {
-            ones += Math.pow(10, n.length-i-2)*(parseInt(str)*(n.length-i-1)+10);
+    for (let i = 0; i < n.length; i++) {
+        let str = n[i];
+        if (str !== "0") {
+            if (str === "1") {
+                ones += parseInt(n) % Math.pow(10, n.length - i - 1) + 1 + (n.length - i - 1) * Math.pow(10, n.length - i - 2);
+            } else {
+                ones += Math.pow(10, n.length - i - 2) * (parseInt(str) * (n.length - i - 1) + 10);
+            }
         }
-    })
+    }
     return ones;
 };
